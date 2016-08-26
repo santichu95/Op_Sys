d55eed598bc9af108128989284685d0cc3de74ba
santichu95 <santiago_andaluz_ruiz@baylor.edu>
2016-08-26 12:04:02 -0500

    First pass through

---
+++ triangle.c
@@ -0,0 +1,39 @@
+/**
+ * Santiago Andaluz Ruiz 
+ * Program 0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char* argv[] ) {
+    if ( argc != 2 ) {
+        write(1, "Wrong number of arguements\n", 
+                strlen("Wrong number of arguements\n"));
+        exit(1);
+    } 
+
+    //Check if the files exists
+
+    //Check if the number of programs is valid
+    //i.e > 0
+    
+    //Read in the points
+
+    //Create Pipe
+    
+    //Fork and thread
+
+    //Figure out how to solve the question.
+
+    //Have a selected point
+    //Check if any two other points have negative reciprocal slopes
+    
+    //for each point {
+    //Double for loop
+    //for i = 0 ... numPoints - 1
+    //  for j = 1 + 1 ... numPoints
+    //  }
+    
+    return 0;
+}
