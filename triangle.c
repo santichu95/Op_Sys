76a8eadcc43fdef0d374ad458c79b46f2c963293
santichu95 <santiago_andaluz_ruiz@baylor.edu>
2016-09-01 20:19:32 -0500

    File and Param checking

--- triangle.c
+++ triangle.c
@@ -7,21 +7,35 @@
 #include <string.h>
 
 int main( int argc, char* argv[] ) {
-    if ( argc != 2 ) {
+    if ( argc != 3 ) {
         write(1, "Wrong number of arguements\n", 
                 strlen("Wrong number of arguements\n"));
         exit(1);
     } 
 
+    FILE* input;
+    int numProc;
+
     //Check if the files exists
+    if( !( fopen(argv[1], "r")) ) {
+        perror("FIle not found");
+    }
 
     //Check if the number of programs is valid
     //i.e > 0
+    numProc = atoi(argv[2]);
+    
+    if( numProc < 0 || numProc > 100 ) {
+        fprintf( stderr, 
+            "Number of processes was not in the acceptable bounds\n", 
+            strlen("Number of processes was not in the acceptable bouds\n"));
+    }
+
     
     //Read in the points
 
     //Create Pipe
-    
+   
     //Fork and thread
 
     //Figure out how to solve the question.
